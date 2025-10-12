8407b4bc8e85729f8dd2a5c40500b6f592646371
Andrew Staus <adstaus@hotmail.com>
2025-10-11 23:59:19 -0600

    fixed deployment script for new structure

--- .vscode/tasks/deploy.ps1
+++ .vscode/tasks/deploy.ps1
@@ -1,20 +1,16 @@
 $env:DESTINATION__PASSWORD = ((Get-Content .env.prod) -match 'DESTINATION__PASSWORD=(.*)').split("=")[1].trim()
-$dbt_path = ".\data_foundation\dbt"
+$dbt_path = ".\packages\data_foundation\dbt"
 $tag = (-join ((97..122) | Get-Random -Count 15 | ForEach-Object {[char]$_}))
-$new_foundation_image = "dagster/data-foundation:"+$tag
-$new_science_image = "dagster/data-science:"+$tag
-$old_foundation_image=docker inspect data-foundation | ConvertFrom-Json
-$old_foundation_image=$old_foundation_image.Config.Image
-$old_science_image=docker inspect data-science | ConvertFrom-Json
-$old_science_image=$old_science_image.Config.Image
+$foundation_image = "dagster/data-foundation:"+$tag
+$science_image = "dagster/data-science:"+$tag
 $defer_path = $dbt_path+"\state\"
 
 Write-Host("`nBUILDING FOUNDATION DOCKER IMAGE")
-docker build . -f Dockerfile.data_foundation --target data_foundation -t $new_foundation_image `
+docker build . -f Dockerfile.data_foundation --target data_foundation -t $foundation_image `
     --secret id=DESTINATION__PASSWORD,env=DESTINATION__PASSWORD
     
 Write-Host("`nBUILDING SCIENCE DOCKER IMAGE")
-docker build . -f Dockerfile.data_science --target data_science -t $new_science_image
+docker build . -f Dockerfile.data_science --target data_science -t $science_image
 
 Write-Host("`nDEPLOYING BUILD")
 winget install Helm.Helm
@@ -39,34 +35,28 @@ kubectl delete pod --field-selector=status.phase==ErrImagePull
 kubectl delete pod --field-selector=status.phase==ImagePullBackoff
 
 # Write-Host("`nCLEANING DOCKER REPOSITORY")
-# docker stop data-foundation
-# docker rm data-foundation
-# docker container create -t --name data-foundation $new_foundation_image
-# docker stop data-foundation-rollback
-# docker rm data-foundation-rollback
-# docker container create -t --name data-foundation-rollback $old_foundation_image
+docker stop data-foundation
+docker rm data-foundation
+docker container create --name data-foundation $foundation_image
 
-# docker stop data-science
-# docker rm data-science
-# docker container create -t --name data-science $new_science_image
-# docker stop data-science-rollback
-# docker rm data-science-rollback
-# docker container create -t --name data-science-rollback $new_science_image
+docker stop data-science
+docker rm data-science
+docker container create -t --name data-science $science_image
 
-# Start-Sleep 5
-# docker image prune -a --force
+Start-Sleep 5
+docker image prune -a --force
 
-# Write-Host("`nDEPLOYMENT COMPLETE")
-# Write-Host("Branch ID: " + $tag + "`n")
+Write-Host("`nDEPLOYMENT COMPLETE")
+Write-Host("Branch ID: " + $tag + "`n")
 
-# Write-Host("`BUILDING DBT Deferal Target")
-# if (!(Test-Path -Path $defer_path -PathType Container)) {
-#     New-Item -Path $defer_path -ItemType Directory
-# }
-# Copy-Item -Path $dbt_path"\target\manifest.json" -Destination $defer_path -Force
+Write-Host("`BUILDING DBT Deferal Target")
+if (!(Test-Path -Path $defer_path -PathType Container)) {
+    New-Item -Path $defer_path -ItemType Directory
+}
+Copy-Item -Path $dbt_path"\target\manifest.json" -Destination $defer_path -Force
 
-# .\data_foundation\.venv\Scripts\activate
-# uv run --env-file .env.prod dbt parse --project-dir $dbt_path --profiles-dir $dbt_path --target prod
+.\packages\data_foundation\.venv\Scripts\activate
+uv run --env-file .env.prod dbt parse --project-dir $dbt_path --profiles-dir $dbt_path --target prod
 
 # # Write-Host("`CREATING PROJECT DOCS") # fusion does not have doc generation yet
 # # uv run --env-file .env.prod dbt docs generate --project-dir $dbt_path --target prod
